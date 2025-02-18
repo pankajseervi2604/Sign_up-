54452add16e574ac8c21e0dc8c82dc1a38986e10
Pankaj Seervi A <pankajseervi2604@gmail.com>
2025-02-18 16:34:35 +0530

    working in firebase authencation and ui practice

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_practice_1/sign_in_page.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: SignInPage(),
+    );
+  }
+}
